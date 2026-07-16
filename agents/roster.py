5c7a6b4cc13612f37b5c4e37f368194321409c20
cccccccccccccccrre33 <eresenkovalentin@gmail.com>
2026-07-16 18:08:47 +0300

    Update roster.py

--- agents/roster.py
+++ agents/roster.py
@@ -15,9 +15,15 @@ from agents.expansion_geniuses import build_global_roster as build_expansion_ros
 from agents.global_elite import GLOBAL_ELITE_1_KEYS, build_global_elite_1_roster
 from agents.global_elite_100 import GLOBAL_ELITE_2_KEYS, build_global_elite_2_roster
 from agents.global_geniuses import build_global_roster
+from agents.gtm import build_gtm_lead
 from agents.growth_team import build_growth_roster
 from agents.specialists import build_specialist_roster
-from agents.squads import build_squad_lead_alpha, build_squad_lead_bravo
+from agents.squads import (
+    build_squad_lead_alpha,
+    build_squad_lead_bravo,
+    build_squad_lead_platform,
+    build_squad_lead_product,
+)
 from agents.team import build_team
 
 CODE_ACCESS_ROLES = {
@@ -25,7 +31,7 @@ CODE_ACCESS_ROLES = {
     "cto", "backend_senior", "product_frontend", "qa_security",
     "mit", "caltech", "stanford", "cmu", "tsinghua", "pku", "ustc", "eth", "kaist",
     "database_engineer", "performance_engineer", "security_engineer", "reliability_engineer",
-    "squad_lead_alpha", "squad_lead_bravo",
+    "squad_lead_alpha", "squad_lead_bravo", "squad_lead_platform", "squad_lead_product",
     "mlops_engineer", "devops_engineer", "product_designer",
     "technion", "polytechnique", "utokyo", "berkeley_mlinfra", "toronto",
     "itmo", "oxford", "iit_bombay", "nus", "waterloo_prod",
@@ -42,8 +48,9 @@ CODE_ACCESS_ROLES = {
 
 
 def build_full_roster() -> dict:
-    """Возвращает dict {role: Agent} со всеми людьми компании (~209:
-    59 исходных + 50 Global Elite I + 100 Global Elite II)."""
+    """Возвращает dict {role: Agent} со всеми людьми компании (~212:
+    59 исходных + 50 Global Elite I + 100 Global Elite II + 2 новых лида
+    отрядов Platform/Product + GTM Lead)."""
     roster = {}
     roster.update(build_board())
     roster.update(build_team())
@@ -58,4 +65,7 @@ def build_full_roster() -> dict:
     roster.update(build_global_elite_2_roster(can_write=False))
     roster["squad_lead_alpha"] = build_squad_lead_alpha()
     roster["squad_lead_bravo"] = build_squad_lead_bravo()
+    roster["squad_lead_platform"] = build_squad_lead_platform()
+    roster["squad_lead_product"] = build_squad_lead_product()
+    roster["gtm_lead"] = build_gtm_lead()
     return roster
